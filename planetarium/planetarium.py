82d416c152048d1349f128f0a9a5626ccf676a10
JdeH <info@geatec.com>
2021-07-01 00:27:27 +0200

    Some designing starting to take place ;)

--- planetarium/planetarium.py
+++ planetarium/planetarium.py
@@ -3,25 +3,54 @@ import pymui as pm
 
 class Planetarium:
     def __init__ (self):
-        pass
+        self.pages = (
+            ('Sky Map', self.drawSkyMap),
+            ('Solar System', self.drawSolarSystem),
+            ('Planet Visibility', self.drawPlanetVisibility)
+        )
+
+        self.labelIndex, self.drawIndex = range (len (self.pages))
+        pa.render (self.el, None, 'root')
+
+    def tabEl (self, index):
+        return pa.el (pm.Tab, {'value': str (index), 'label': self.pages [index][self.labelIndex], 'onClick': lambda: self.setPageIndex (index)})
+
+    def tabPanelEl (self, index):
+        return pa.el (pm.TabPanel, {'value': str (index)}, pa.el ('canvas', {'ref': self.canvasRef}))
 
     def el (self):
-        tabId, setTabId = pa.us ('sm')
+        self.canvasRef = pa.createRef ()
+        self.pageIndex, self.setPageIndex = pa.useState (0)
+        pa.useEffect (self.render)
 
         return pa.el ('div', None,
-            pa.el (pm.TabContext, {'value': tabId},
+            pa.el (pm.TabContext, {'value': str (self.pageIndex)},
                 pa.el (pm.AppBar, {'position': 'static'},
-                    pa.el (pm.Tabs, {'value': tabId},
-                        pa.el (pm.Tab, {'value': 'sm', 'label': 'Sky Map', 'onClick': lambda: setTabId ('sm')}),
-                        pa.el (pm.Tab, {'value': 'ss', 'label': 'Solar System', 'onClick': lambda: setTabId ('ss')}),
-                        pa.el (pm.Tab, {'value': 'pv', 'label': 'Planet Visibility', 'onClick': lambda: setTabId ('pv')})
+                    pa.el (pm.Tabs, {'value': str (self.pageIndex)},
+                        *(self.tabEl (index) for index in range (len (self.pages)))
                     )
                 ),
-                pa.el (pm.TabPanel, {'value': 'sm'}, 'The Sky Map'),
-                pa.el (pm.TabPanel, {'value': 'ss'}, 'The Solar System'),
-                pa.el (pm.TabPanel, {'value': 'pv'}, pa.el ('canvas'))
+                *(self.tabPanelEl (index) for index in range (len (self.pages))),
             )
         )
 
+    def render (self):
+        self.context = self.canvasRef.current.getContext ('2d')
+        self.pages [self.pageIndex][self.drawIndex]()
+
+    def drawSkyMap (self):
+        self.context.beginPath ()
+        self.context.rect (20, 20, 20, 20)
+        self.context.stroke ()
+    
+    def drawSolarSystem (self):
+        self.context.beginPath ()
+        self.context.rect (20, 20, 40, 40)
+        self.context.stroke ()
+    
+    def drawPlanetVisibility (self):
+        self.context.beginPath ()
+        self.context.rect (20, 20, 60, 60)
+        self.context.stroke ()
+    
 planetarium = Planetarium ()
-pa.render (planetarium.el, None, 'root')
